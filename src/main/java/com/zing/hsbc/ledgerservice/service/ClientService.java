e451c751940c3c9eacf2e3579fe9dda2ec77297d
daniel <daniellinws@gmail.com>
2024-03-17 15:33:58 +0800

    add posting listener

--- src/main/java/com/zing/hsbc/ledgerservice/service/ClientService.java
+++ src/main/java/com/zing/hsbc/ledgerservice/service/ClientService.java
@@ -4,6 +4,8 @@ import com.zing.hsbc.ledgerservice.entity.Client;
 import com.zing.hsbc.ledgerservice.entity.TransactionQuery;
 import com.zing.hsbc.ledgerservice.entity.WalletSubscription;
 import com.zing.hsbc.ledgerservice.repo.ClientRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -11,10 +13,12 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-import static com.zing.hsbc.ledgerservice.notification.KafkaTopic.TOPIC_BALANCE_CHANGED;
+import static com.zing.hsbc.ledgerservice.notification.KafkaTopic.*;
 
 @Service
 public class ClientService {
+    private static final Logger log = LoggerFactory.getLogger(ClientService.class);
+
     @Autowired
     private TransactionQueryService transactionQueryService;
     @Autowired
@@ -40,5 +44,15 @@ public class ClientService {
 
     public Optional<Client> findById(Long id){
         return clientRepository.findById(id);
+    }
+
+    @KafkaListener(topics = TOPIC_POSTING_CLEAR)
+    public void transactionClearListener(List<Long> transactionIds) {
+        log.info("transactionClearListener called");
+    }
+
+    @KafkaListener(topics = TOPIC_POSTING_FAILED)
+    public void transactionFailedListener(List<Long> transactionIds) {
+        log.info("transactionFailedListener called");
     }
 }
