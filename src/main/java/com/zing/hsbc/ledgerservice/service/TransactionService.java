96150358091231195b95fbcea7444caf301b7d00
daniel <daniellinws@gmail.com>
2024-03-17 20:18:44 +0800

    bug fixing

--- src/main/java/com/zing/hsbc/ledgerservice/service/TransactionService.java
+++ src/main/java/com/zing/hsbc/ledgerservice/service/TransactionService.java
@@ -22,11 +22,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -48,11 +53,16 @@ public class TransactionService {
     AccountService accountService;
     @Autowired
     TransactionQueryService transactionQueryService;
+    @Autowired
+    PlatformTransactionManager transactionManager;
 
     // Handles incoming Kafka messages containing transaction IDs for processing.
     // Initiates the transaction processing workflow for each received ID.
     @KafkaListener(topics = TOPIC_POSTING_PROCESS)
-    public void processTransactions(List<Long> transactionIds) {
+    public void processTransactions(String idsString) {
+        List<Long> transactionIds = Arrays.stream(idsString.split(","))
+                .map(Long::parseLong)
+                .collect(Collectors.toList());
         log.info("Starting to process transactions with IDs: {}", transactionIds);
         List<Transaction> transactions = transactionRepository.findAllById(transactionIds).stream().collect(Collectors.toUnmodifiableList());
         try {
@@ -65,65 +75,85 @@ public class TransactionService {
 
     // Processes a list of transactions by executing financial operations between source and target wallets.
     // Ensures sufficient funds, updates wallet balances, and records transactions as either CLEAR or FAILED.
-    @Transactional
-    public void doProcessTransactions(List<Transaction> transactions) throws JsonProcessingException {
-        log.info("Processing {} transactions.", transactions.size());
-        List<TransactionQuery> transactionQueries = new ArrayList<>();
-        for (Transaction transaction : transactions) {
-            Wallet sourceWallet = walletService.getWallet(transaction.getSourceWalletId()).orElseThrow(() -> new ResourceNotFoundException("Source account not found for transaction ID " + transaction.getTransactionId()));
-            Wallet targetWallet = walletService.getWallet(transaction.getTargetWalletId()).orElseThrow(() -> new ResourceNotFoundException("Target account not found for transaction ID " + transaction.getTransactionId()));
-            BigDecimal sourceBalanceBefore = sourceWallet.getBalance();
-            BigDecimal targetBalanceBefore = targetWallet.getBalance();
+    public void doProcessTransactions(List<Transaction> transactions) {
+        DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+        def.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+        TransactionStatus status = transactionManager.getTransaction(def);
+        try {
+            log.info("Processing {} transactions.", transactions.size());
+            List<TransactionQuery> transactionQueries = new ArrayList<>();
+            for (Transaction transaction : transactions) {
+                Wallet sourceWallet = walletService.getWallet(transaction.getSourceWalletId()).orElseThrow(() -> new ResourceNotFoundException("Source account not found for transaction ID " + transaction.getTransactionId()));
+                Wallet targetWallet = walletService.getWallet(transaction.getTargetWalletId()).orElseThrow(() -> new ResourceNotFoundException("Target account not found for transaction ID " + transaction.getTransactionId()));
+                BigDecimal sourceBalanceBefore = sourceWallet.getBalance();
+                BigDecimal targetBalanceBefore = targetWallet.getBalance();
 
-            if (sourceWallet.getBalance().compareTo(transaction.getAmount()) >= 0) {
-                sourceWallet.setBalance(sourceWallet.getBalance().subtract(transaction.getAmount()));
-                targetWallet.setBalance(targetWallet.getBalance().add(transaction.getAmount()));
-                log.debug("Transferred {} from wallet {} to wallet {}.", transaction.getAmount(), transaction.getSourceWalletId(), transaction.getTargetWalletId());
-            } else {
-                log.debug("Insufficient funds in source wallet {} for transaction ID {}", transaction.getSourceWalletId(), transaction.getTransactionId());
-                throw new InsufficientFundException("Insufficient funds in source wallet for transaction ID " + transaction.getTransactionId());
+                if (sourceWallet.getBalance().compareTo(transaction.getAmount()) >= 0) {
+                    sourceWallet.setBalance(sourceWallet.getBalance().subtract(transaction.getAmount()));
+                    targetWallet.setBalance(targetWallet.getBalance().add(transaction.getAmount()));
+                    log.debug("Transferred {} from wallet {} to wallet {}.", transaction.getAmount(), transaction.getSourceWalletId(), transaction.getTargetWalletId());
+                } else {
+                    log.debug("Insufficient funds in source wallet {} for transaction ID {}", transaction.getSourceWalletId(), transaction.getTransactionId());
+                    throw new InsufficientFundException("Insufficient funds in source wallet for transaction ID " + transaction.getTransactionId());
+                }
+
+                walletService.createOrUpdateWallet(sourceWallet);
+                walletService.createOrUpdateWallet(targetWallet);
+                transaction.setState(TransactionState.CLEAR);
+                transaction.setTransactionDate(LocalDateTime.now());
+                TransactionQuery transactionQuery = Utils.createTransactionQueryFromTransactionAndWallets(transaction, sourceWallet, targetWallet, sourceBalanceBefore, targetBalanceBefore);
+                transactionQueries.add(transactionQuery);
+                TransactionClearEvent event = new TransactionClearEvent(transaction.getTransactionId(), transactionQuery);
+                eventStoreService.saveEvent(transaction.getTransactionId(), event);
             }
 
-            walletService.createOrUpdateWallet(sourceWallet);
-            walletService.createOrUpdateWallet(targetWallet);
-            transaction.setState(TransactionState.CLEAR);
-            transaction.setTransactionDate(LocalDateTime.now());
-            TransactionQuery transactionQuery = Utils.createTransactionQueryFromTransactionAndWallets(transaction, sourceWallet, targetWallet, sourceBalanceBefore, targetBalanceBefore);
-            transactionQueries.add(transactionQuery);
-            TransactionClearEvent event = new TransactionClearEvent(transaction.getTransactionId(), transactionQuery);
-            eventStoreService.saveEvent(transaction.getTransactionId(), event);
+            transactionRepository.saveAll(transactions);
+            notificationProducer.sendClearTransaction(transactionQueries);
+            notificationProducer.sendBalanceChangedMsg(transactions);
+            log.info("Completed processing transactions.");
+            transactionManager.commit(status);
+        } catch (Exception ex) {
+            if (!status.isCompleted()) {
+                transactionManager.rollback(status);
+            }
+            throw new RuntimeException(ex);
         }
-
-        transactionRepository.saveAll(transactions);
-        notificationProducer.sendClearTransaction(transactionQueries);
-        notificationProducer.sendBalanceChangedMsg(transactions);
-        log.info("Completed processing transactions.");
     }
 
     // Handles exceptions encountered during transaction processing.
     // Marks affected transactions as FAILED, updates their status, and notifies the necessary parties.
-    @Transactional
     public void handleErrorOccur(List<Transaction> transactions) {
-        log.warn("Handling error for {} transactions.", transactions.size());
-        LocalDateTime now = LocalDateTime.now();
-        transactions.forEach(transaction -> {
-            transaction.setState(TransactionState.FAILED);
-            transaction.setTransactionDate(now);
-            log.info("Marked transaction ID {} as FAILED.", transaction.getTransactionId());
-            TransactionQuery transactionQuery = Utils.createTransactionQueryFromTransaction(transaction);
-            transactionQueryService.save(transactionQuery);
-        });
-        transactionRepository.saveAll(transactions);
-        notificationProducer.sendFailedTransaction(transactions);
-        transactions.forEach(transaction -> {
-            try {
-                TransactionFailedEvent event = new TransactionFailedEvent(transaction.getTransactionId(), transaction);
-                eventStoreService.saveEvent(transaction.getTransactionId(), event);
-            } catch (JsonProcessingException e) {
-                log.error("Failed to serialize failed event for transaction ID {}: ", transaction.getTransactionId(), e);
-                throw new RuntimeException(e);
+        DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+        def.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+        TransactionStatus status = transactionManager.getTransaction(def);
+        try {
+            log.warn("Handling error for {} transactions.", transactions.size());
+            LocalDateTime now = LocalDateTime.now();
+            transactions.forEach(transaction -> {
+                transaction.setState(TransactionState.FAILED);
+                transaction.setTransactionDate(now);
+                log.info("Marked transaction ID {} as FAILED.", transaction.getTransactionId());
+                TransactionQuery transactionQuery = Utils.createTransactionQueryFromTransaction(transaction);
+                transactionQueryService.save(transactionQuery);
+            });
+            transactionRepository.saveAll(transactions);
+            notificationProducer.sendFailedTransaction(transactions);
+            transactions.forEach(transaction -> {
+                try {
+                    TransactionFailedEvent event = new TransactionFailedEvent(transaction.getTransactionId(), transaction);
+                    eventStoreService.saveEvent(transaction.getTransactionId(), event);
+                } catch (JsonProcessingException e) {
+                    log.error("Failed to serialize failed event for transaction ID {}: ", transaction.getTransactionId(), e);
+                    throw new RuntimeException(e);
+                }
+            });
+            transactionManager.commit(status);
+        } catch (Exception ex) {
+            if (!status.isCompleted()) {
+                transactionManager.rollback(status);
             }
-        });
+            throw new RuntimeException(ex);
+        }
     }
 
     // Creates and persists new transactions based on input, generating necessary events for each.
