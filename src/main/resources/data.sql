cd5ad963c1a827fa2e293324c46bb1f405904629
daniel <daniellinws@gmail.com>
2024-03-17 15:19:27 +0800

    added testing page

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,11 @@
-insert into client(name) values ('Central Bank');
-insert into account(client_id, name, state) values (1, 'saving', 'ACTIVE');
-insert into wallet (balance, asset, account_id) values (1000, 'USD', 1);
-insert into wallet (balance, asset, account_id) values (1000, 'EUR', 1);
-
 insert into client(name) values ('Zing');
+insert into account(client_id, name, state) values (1, 'saving', 'ACTIVE');
+insert into wallet (balance, asset, account_id) values (10000, 'USD', 1);
+insert into wallet (balance, asset, account_id) values (10000, 'EUR', 1);
+
+insert into client(name) values ('Client A');
 insert into account(client_id, name, state) values (2, 'investment', 'ACTIVE');
 insert into wallet (balance, asset, account_id) values (0, 'USD', 2);
 insert into wallet (balance, asset, account_id) values (0, 'EUR', 2);
+
+insert into client(name) values ('Client B');
